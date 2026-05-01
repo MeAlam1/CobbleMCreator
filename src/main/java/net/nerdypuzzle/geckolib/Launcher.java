6ced4687a52da8880e31920d7dc2066833c86d90
MeAlam1 <schonhagearam@gmail.com>
2026-05-01 15:50:33 +0200

    Cleanup

---
+++ src/main/java/net/nerdypuzzle/geckolib/Launcher.java
@@ -0,0 +1,24 @@
+package net.nerdypuzzle.geckolib;
+
+import net.mcreator.plugin.JavaPlugin;
+import net.mcreator.plugin.Plugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Launcher extends JavaPlugin {
+
+	public static final Logger LOG = LogManager.getLogger("Cobblemon Plugin");
+	public static Set<Plugin> PLUGIN_INSTANCE = new HashSet<>();
+
+	public Launcher(Plugin pPlugin) {
+		super(pPlugin);
+		PLUGIN_INSTANCE.add(pPlugin);
+
+		LOG.info("Plugin was loaded");
+	}
+
+
+}
